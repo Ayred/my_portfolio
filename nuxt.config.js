0ed8f857c3b1335be14278298710069bcc63e485
Ayred Antara Caicedo Torres <ayredantara@gmail.com>
2022-11-09 10:20:00 -0800

    change port from 3000 to 3001

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,30 +11,36 @@ export default {
       {
         hid: 'description',
         name: 'description',
-        content: 'This is an awesome description of my Nuxt app',
-      },
+        content: 'This is an awesome description of my Nuxt app'
+      }
     ],
     link: [
-	{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-	{ rel: "stylesheet", href: "http://fonts.googleapis.com/css2?family=Jost:wght@300;400;600&display=swap"},
-	{ rel: "stylesheet", href: "/resources/assets/css/style-starter.css"},
-
-	],
-	script: [
-    { src: "/resources/assets/js/customjs1.js", body: true },
-    { src: "/resources/assets/js/theme-change.js", body: true },
-    { src: "/resources/assets/js/jquery-3.3.1.min.js", body: true },
-    { src: "/resources/assets/js/customjs2.js", body: true },
-    { src: "/resources/assets/js/owl.carousel.js", body: true },
-    { src: "/resources/assets/js/customjs3.js", body: true },
-    { src: "/resources/assets/js/jquery.magnific-popup.min.js", body: true },
-    { src: "/resources/assets/js/customjs4.js", body: true },
-    { src: "/resources/assets/js/jquery.waypoints.min.js", body: true },
-    { src: "/resources/assets/js/jquery.countup.js", body: true },
-    { src: "/resources/assets/js/customjs5.js", body: true },
-    { src: "/resources/assets/js/bootstrap.min.js", body: true },
-
-  ],
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'stylesheet',
+        href: 'http://fonts.googleapis.com/css2?family=Jost:wght@300;400;600&display=swap'
+      },
+      { rel: 'stylesheet', href: '/resources/assets/css/style-starter.css' }
+    ],
+    script: [
+      { src: '/resources/assets/js/customjs1.js', body: true },
+      { src: '/resources/assets/js/theme-change.js', body: true },
+      { src: '/resources/assets/js/jquery-3.3.1.min.js', body: true },
+      { src: '/resources/assets/js/customjs2.js', body: true },
+      { src: '/resources/assets/js/owl.carousel.js', body: true },
+      { src: '/resources/assets/js/customjs3.js', body: true },
+      { src: '/resources/assets/js/jquery.magnific-popup.min.js', body: true },
+      { src: '/resources/assets/js/customjs4.js', body: true },
+      { src: '/resources/assets/js/jquery.waypoints.min.js', body: true },
+      { src: '/resources/assets/js/jquery.countup.js', body: true },
+      { src: '/resources/assets/js/customjs5.js', body: true },
+      { src: '/resources/assets/js/bootstrap.min.js', body: true }
+    ]
+  },
+  server: {
+    port: 3001,
+    host: '0.0.0.0', // default: localhost,
+    timing: false
   },
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
